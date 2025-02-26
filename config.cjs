01406f520dbbaa4065fda05f3a20246997a55a68
nobodyfrl <91120336+Spidersenpai@users.noreply.github.com>
2025-02-26 16:20:24 -0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || " Sarkarmd$NdzGTr7a",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
@@ -20,9 +20,9 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "private",
-  OWNER_NAME: process.env.OWNER_NAME || "©Bandaheali",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "923253617422",
+  MODE: process.env.MODE || "public",
+  OWNER_NAME: process.env.OWNER_NAME || "©Orrupt",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "18763122982",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
